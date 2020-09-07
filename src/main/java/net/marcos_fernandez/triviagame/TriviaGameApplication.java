13b95e7de72d858e4a34b1db7f4de802494d7077
marcos.fernandez <Julio2020>
2020-09-07 19:03:24 +0200

    - Added empty Spring Boot project with expected dependencies

---
+++ src/main/java/net/marcos_fernandez/triviagame/TriviaGameApplication.java
@@ -0,0 +1,13 @@
+package net.marcos_fernandez.triviagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/marcos_fernandez/triviagame/TriviaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package net.marcos_fernandez.triviagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
